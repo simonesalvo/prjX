168136a1bc99b773c2ce2b6a6a64bfabcd696a7e
simone_salvo <simone.salvo@ymail.com>
2015-12-19 22:02:58 +0100

    * Adding Cruds

---
+++ src/main/java/it/simonesalvo/prjX/servlet/good/crud/read.java
@@ -0,0 +1,23 @@
+package it.simonesalvo.prjX.servlet.good.crud;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Simone Salvo on 19/12/15.
+ * www.simonesalvo.it
+ */
+
+public class read extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
+
+
+
